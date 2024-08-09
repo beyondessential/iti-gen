4b19ecb6306aa474a1dc54a7ed9ee465aa809681
Félix Saparelli <felix@passcod.name>
2024-08-09 16:56:39 +1200

    be more resistant to reruns

--- stage2/05-firmware/00-run.sh
+++ stage2/05-firmware/00-run.sh
@@ -2,7 +2,7 @@
 
 # remove boot script (it's for the minimal image only)
 on_chroot << EOF
-systemctl disable iti-boot-config.timer
+systemctl disable iti-boot-config.timer || true
 EOF
 rm -f "${ROOTFS_DIR}/usr/local/bin/iti-boot-config"
 rm -f "${ROOTFS_DIR}/etc/systemd/system/iti-boot-config.service"
