4e5cfd2b3b2ed7def52a8e5ace68c9465daaec1a
Félix Saparelli <felix@passcod.name>
2024-08-09 16:56:52 +1200

    apt update after adding source

--- stage4/07-tailscale/01-run.sh
+++ stage4/07-tailscale/01-run.sh
@@ -5,3 +5,7 @@ install -vm600 files/*.asc "${ROOTFS_DIR}/etc/apt/keyrings/"
 cat >"${ROOTFS_DIR}/etc/apt/sources.list.d/tailscale.list" <<TSDEB
 deb [signed-by=/etc/apt/keyrings/tailscale.asc] https://pkgs.tailscale.com/stable/debian $(. /etc/os-release && echo "$VERSION_CODENAME") main
 TSDEB
+
+on_chroot << EOF
+  apt update
+EOF
