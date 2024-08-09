eaca6b6b664fa48fb43ce53d1b6d08bf2ad231b6
Félix Saparelli <felix@passcod.name>
2024-08-09 20:21:58 +1200

    fix source for tailscale

--- stage4/07-tailscale/01-run.sh
+++ stage4/07-tailscale/01-run.sh
@@ -3,7 +3,7 @@
 install -vm600 files/*.asc "${ROOTFS_DIR}/etc/apt/keyrings/"
 
 cat >"${ROOTFS_DIR}/etc/apt/sources.list.d/tailscale.list" <<TSDEB
-deb [signed-by=/etc/apt/keyrings/tailscale.asc] https://pkgs.tailscale.com/stable/debian $(. /etc/os-release && echo "$VERSION_CODENAME") main
+deb [signed-by=/etc/apt/keyrings/tailscale.asc] https://pkgs.tailscale.com/stable/debian $RELEASE main
 TSDEB
 
 on_chroot << EOF
