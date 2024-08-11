54a9d888932b6fc4a37cdbcd67d151f18159e63e
Félix Saparelli <felix@passcod.name>
2024-08-12 10:48:51 +1200

    fix apt key handling

--- stage4/07-tailscale/01-run.sh
+++ stage4/07-tailscale/01-run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-install -vm600 files/*.asc "${ROOTFS_DIR}/etc/apt/keyrings/"
+install -vm644 files/*.asc "${ROOTFS_DIR}/etc/apt/keyrings/"
 
 cat >"${ROOTFS_DIR}/etc/apt/sources.list.d/tailscale.list" <<TSDEB
 deb [signed-by=/etc/apt/keyrings/tailscale.asc] https://pkgs.tailscale.com/stable/debian $RELEASE main
